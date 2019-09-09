bf616933e27ec5ad4c39b0a35c2fd2561ed35f6f
thebeardphantom <michael@beardphantom.com>
2019-09-08 19:41:16 -0700

    Init commit

---
+++ Source/Runtime/Bootstrapper.cs
@@ -0,0 +1,153 @@
+﻿using System.Collections.Generic;
+using RSG;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+using UnityEngine.SceneManagement;
+#if UNITY_EDITOR
+using UnityEngine;
+
+#endif
+
+namespace ASFUnity.Core.Runtime
+{
+    public abstract class Bootstrapper : MonoBehaviour
+    {
+        #region Types
+
+        public enum BootstrapperState
+        {
+            WaitingOnBootstrap,
+            Complete
+        }
+
+        #endregion
+
+        #region Fields
+
+        private const string BOOTSTRAPPER_LOADED_SCENES = "BOOTSTRAPPER_LOADED_SCENES";
+
+        [Min(0)]
+        [SerializeField]
+        private int _bootstrapSceneBuildIndex;
+
+        #endregion
+
+        #region Properties
+
+        public static Bootstrapper Instance { get; private set; }
+
+        public IPromise BootstrapPromise { get; private set; }
+
+        public BootstrapperState State { get; private set; } = BootstrapperState.Complete;
+
+        #endregion
+
+        #region Methods
+
+        protected abstract IPromise GetBootstrapTasks();
+
+        public void BeginBootstrapping()
+        {
+            if (State != BootstrapperState.Complete)
+            {
+                return;
+            }
+
+            State = BootstrapperState.WaitingOnBootstrap;
+            BootstrapPromise = GetBootstrapTasks();
+            BootstrapPromise.Done(
+                () =>
+                {
+                    State = BootstrapperState.Complete;
+                    RestoreDesiredScenes();
+                });
+        }
+
+        protected virtual void Awake()
+        {
+            if (EnsureInstance())
+            {
+                StoreDesiredScenes();
+                SceneManager.LoadScene(_bootstrapSceneBuildIndex);
+            }
+        }
+
+        protected virtual void Start()
+        {
+            if (EnsureInstance())
+            {
+                BeginBootstrapping();
+            }
+        }
+
+        protected void OnValidate()
+        {
+            transform.parent = null;
+            transform.SetPositionAndRotation(Vector3.zero, Quaternion.identity);
+            transform.SetAsFirstSibling();
+            name = "--BOOTSTRAP--";
+        }
+
+        protected virtual void Reset()
+        {
+            OnValidate();
+        }
+
+        private bool EnsureInstance()
+        {
+            if (Instance != null && Instance != this)
+            {
+                Destroy(gameObject);
+                return false;
+            }
+
+            Instance = this;
+            DontDestroyOnLoad(this);
+            return true;
+        }
+
+        private void StoreDesiredScenes()
+        {
+#if UNITY_EDITOR
+            var scenePaths = new List<string>();
+            for (var i = 0; i < SceneManager.sceneCount; i++)
+            {
+                var scene = SceneManager.GetSceneAt(i);
+                if (scene.buildIndex != _bootstrapSceneBuildIndex)
+                {
+                    scenePaths.Add(scene.path);
+                }
+            }
+
+            var scenesString = string.Join("\n", scenePaths);
+            SessionState.SetString(BOOTSTRAPPER_LOADED_SCENES, scenesString);
+#endif
+        }
+
+        private void RestoreDesiredScenes()
+        {
+#if UNITY_EDITOR
+            var desiredScenesStr = SessionState.GetString(BOOTSTRAPPER_LOADED_SCENES, null);
+            if (desiredScenesStr == null)
+            {
+                SceneManager.LoadScene(_bootstrapSceneBuildIndex + 1);
+            }
+            else
+            {
+                var desiredScenePaths = desiredScenesStr.Split('\n');
+                for (var i = 0; i < desiredScenePaths.Length; i++)
+                {
+                    var path = desiredScenePaths[i];
+                    EditorSceneManager.LoadSceneInPlayMode(
+                        path,
+                        new LoadSceneParameters(i == 0 ? LoadSceneMode.Single : LoadSceneMode.Additive));
+                }
+            }
+#else
+            SceneManager.LoadScene(_bootstrapSceneBuildIndex + 1);
+#endif
+        }
+
+        #endregion
+    }
+}
---
+++ Source/Runtime/Services/ServiceLocation.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using UnityEngine.Assertions;
+
+namespace BeardPhantom.UCL.Services
+{
+    public static class ServiceLocation
+    {
+        #region Fields
+
+        /// <summary>
+        /// Accessor for current service locator
+        /// </summary>
+        private static ServiceKernel _kernel;
+
+        #endregion
+
+        #region Properties
+
+        /// <summary>
+        /// Guid used for tracking changes to the ServiceKernel
+        /// </summary>
+        public static Guid KernelGuid { get; private set; }
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// Populates a field's value using the ServiceKernel
+        /// </summary>
+        public static void Populate<T>(ref T field) where T : class
+        {
+            field = Get<T>();
+        }
+
+        /// <summary>
+        /// Populates two fields' values using the ServiceKernel
+        /// </summary>
+        public static void Populate<T1, T2>(ref T1 field1, ref T2 field2)
+            where T1 : class
+            where T2 : class
+        {
+            field1 = Get<T1>();
+            field2 = Get<T2>();
+        }
+
+        /// <summary>
+        /// Populates three fields' values using the ServiceKernel
+        /// </summary>
+        public static void Populate<T1, T2, T3>(ref T1 field1, ref T2 field2, ref T3 field3)
+            where T1 : class
+            where T2 : class
+            where T3 : class
+        {
+            field1 = Get<T1>();
+            field2 = Get<T2>();
+            field3 = Get<T3>();
+        }
+
+        /// <summary>
+        /// Populates four fields' values using the ServiceKernel
+        /// </summary>
+        public static void Populate<T1, T2, T3, T4>(ref T1 field1, ref T2 field2, ref T3 field3, ref T4 field4)
+            where T1 : class
+            where T2 : class
+            where T3 : class
+            where T4 : class
+        {
+            field1 = Get<T1>();
+            field2 = Get<T2>();
+            field3 = Get<T3>();
+            field4 = Get<T4>();
+        }
+
+        public static void SetKernel(ServiceKernel kernel)
+        {
+            Assert.IsNotNull(kernel);
+
+            _kernel?.Dispose();
+            _kernel = kernel;
+            KernelGuid = Guid.NewGuid();
+            kernel.BindAllModules();
+        }
+
+        public static T Get<T>() where T : class
+        {
+            return _kernel.Get<T>();
+        }
+
+        #endregion
+    }
+}
