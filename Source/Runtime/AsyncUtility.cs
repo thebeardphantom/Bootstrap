d80fca50fb9ccd07992a1880cc41d77b2f117034
thebeardphantom <michael@beardphantom.com>
2019-09-15 19:32:14 -0700

    Added AsyncUtility

---
+++ Source/Runtime/AsyncUtility.cs
@@ -0,0 +1,33 @@
+﻿using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.ResourceManagement.AsyncOperations;
+
+namespace ASF.Core.Runtime
+{
+    public static class AsyncUtility
+    {
+        #region Methods
+
+        public static Task<AsyncOperation> ToTask(this AsyncOperation asyncOp)
+        {
+            var taskCompletion = new TaskCompletionSource<AsyncOperation>();
+            asyncOp.completed += operation =>
+            {
+                taskCompletion.SetResult(operation);
+            };
+            return taskCompletion.Task;
+        }
+
+        public static Task<AsyncOperationHandle<T>> ToTask<T>(this AsyncOperationHandle<T> asyncOp)
+        {
+            var taskCompletion = new TaskCompletionSource<AsyncOperationHandle<T>>();
+            asyncOp.Completed += operation =>
+            {
+                taskCompletion.SetResult(operation);
+            };
+            return taskCompletion.Task;
+        }
+
+        #endregion
+    }
+}
