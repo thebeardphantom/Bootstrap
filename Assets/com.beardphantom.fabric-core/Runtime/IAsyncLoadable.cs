985027babc6ad9b3e7f289165ddc19338d5398f8
Michael Notarnicola <michael@beardphantom.com>
2020-04-18 14:05:06 -0700

    Add IAsyncLoadable

---
+++ Assets/com.beardphantom.fabric-core/Runtime/IAsyncLoadable.cs
@@ -0,0 +1,22 @@
+﻿using UniRx.Async;
+
+namespace Fabric.PersistSim.Runtime
+{
+    public interface IAsyncLoadable
+    {
+        #region Methods
+
+        UniTask LoadAsync();
+
+        #endregion
+    }
+
+    public interface IAsyncLoadable<T>
+    {
+        #region Methods
+
+        UniTask<T> LoadAsync();
+
+        #endregion
+    }
+}
