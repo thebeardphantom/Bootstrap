ac298fd99f26c76d9cd4ca22f01cc7035630aac8
Mika Notarnicola <mika@beardphantom.com>
2021-03-12 02:08:17 -0800

    Add log tags

---
+++ Assets/com.beardphantom.fabric-core/Runtime/LogTags.cs
@@ -0,0 +1,11 @@
+﻿namespace BeardPhantom.Fabric.Core
+{
+    public static class LogTags
+    {
+        #region Fields
+
+        public const string FABRIC_CORE = "Fabric.Core";
+
+        #endregion
+    }
+}
