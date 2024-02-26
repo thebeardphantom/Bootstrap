9d1d0d3e93f7c5a58a758d7d81170d078bb092ea
Mika Notarnicola <mika@beardphantom.com>
2024-02-25 23:25:50 -0800

    Fix for IsPlaying being false

---
+++ Packages/com.beardphantom.bootstrap/Editor/IsPlayingHelper.cs
@@ -0,0 +1,17 @@
+﻿using UnityEditor;
+
+namespace BeardPhantom.Bootstrap
+{
+    internal static class IsPlayingHelper
+    {
+        #region Methods
+
+        [InitializeOnEnterPlayMode]
+        private static void OnPlaymodeStateChanged()
+        {
+            App.IsPlaying = true;
+        }
+
+        #endregion
+    }
+}
