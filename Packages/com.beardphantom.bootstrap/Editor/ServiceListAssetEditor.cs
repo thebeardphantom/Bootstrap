a32eb542c3e6e060ab4e9fef7840ea29936dfb04
Mika Notarnicola <mika@beardphantom.com>
2026-05-16 03:55:38 -0700

    Add button to remove managed references with missing types for ServicesListAsset editor.

---
+++ Packages/com.beardphantom.bootstrap/Editor/ServiceListAssetEditor.cs
@@ -0,0 +1,52 @@
+using System.Linq;
+using UnityEditor;
+using UnityEditor.UIElements;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace BeardPhantom.Bootstrap.Editor
+{
+    [CustomEditor(typeof(ServiceListAsset))]
+    public class ServiceListAssetEditor : UnityEditor.Editor
+    {
+        private const string HelpBoxTxt = "There are SerializeReference types which are missing.";
+        private VisualElement _rootVisualElement;
+
+        public override VisualElement CreateInspectorGUI()
+        {
+            _rootVisualElement = new VisualElement();
+            RecreateUI();
+            return _rootVisualElement;
+        }
+
+        private void RecreateUI()
+        {
+            _rootVisualElement.Clear();
+            bool hasManagedReferencesWithMissingTypes = targets.Any(SerializationUtility.HasManagedReferencesWithMissingTypes);
+            if (hasManagedReferencesWithMissingTypes)
+            {
+                _rootVisualElement.Add(new HelpBox(HelpBoxTxt, HelpBoxMessageType.Warning));
+                _rootVisualElement.Add(
+                    new Button(OnClickRemoveMissingManagedReferencesButton)
+                    {
+                        text = "Remove Missing Managed References",
+                    });
+            }
+
+            InspectorElement.FillDefaultInspector(_rootVisualElement, serializedObject, this);
+            _rootVisualElement.Bind(serializedObject);
+        }
+
+        private void OnClickRemoveMissingManagedReferencesButton()
+        {
+            foreach (Object t in targets)
+            {
+                SerializationUtility.ClearAllManagedReferencesWithMissingTypes(t);
+                EditorUtility.SetDirty(t);
+                AssetDatabase.SaveAssetIfDirty(t);
+            }
+
+            RecreateUI();
+        }
+    }
+}
